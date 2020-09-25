4713fa184c517e11d3bf15ab1019eafadfd92bad
itzhangbao <itzhangbao@163.com>
2020-09-25 15:56:58 +0800

    🚀 Jumbo`home.

--- application/babel/templates/home.js
+++ application/babel/templates/home.js
@@ -13,45 +13,51 @@ function home() {
     var update_time = $('.template[data-template=home] update_time')
     // 获取文件最后修改时间
     var strtime = document.lastModified
+    var lastupdate = new Date(strtime)
     // 将时间转换为date
-    var date = getLocalTime(8, new Date(strtime))
+    var date = getLocalTime(8, lastupdate)
 
+    console.log(date)
+    // 
     Y = date.getFullYear() + '-';
     M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
     D = date.getDate() + ' ';
     h = date.getHours() + ':';
     m = date.getMinutes() + ':';
     s = date.getSeconds();
-    console.log(date)
-    console.log(Y + M + D + h + m + s); //呀麻碟
     $(update_time).text(Y + M + D + h + m + s)
 
-    console.log("*******************东区时间************************************");
-    console.log("零时区-伦敦时间：" + getLocalTime(0));
-    console.log("东一区-柏林时间：" + getLocalTime(1));
-    console.log("东二区-雅典时间：" + getLocalTime(2));
-    console.log("东三区-莫斯科时间：" + getLocalTime(3));
-    console.log("东四区-时间：" + getLocalTime(4));
-    console.log("东五区-伊斯兰堡时间：" + getLocalTime(5));
-    console.log("东六区-科伦坡时间：" + getLocalTime(6));
-    console.log("东七区-曼谷时间：" + getLocalTime(7));
-    console.log("东八区-北京时间：" + getLocalTime(8));
-    console.log("东九区-东京时间：" + getLocalTime(9));
-    console.log("东十区-悉尼时间：" + getLocalTime(10));
-    console.log("东十二区-斐济时间：" + getLocalTime(12));
-    console.log("*******************西区时间************************************");
-    console.log("西十区-斐济时间：" + getLocalTime(-10));
-    console.log("西九区-阿拉斯加时间：" + getLocalTime(-9));
-    console.log("西八区-太平洋时间（美国和加拿大）：" + getLocalTime(-8));
-    console.log("西七区-山地时间（美国和加拿大）：" + getLocalTime(-7));
-    console.log("西六区-中部时间（美国和加拿大）：" + getLocalTime(-6));
-    console.log("西五区-东部时间（美国和加拿大）：" + getLocalTime(-5));
-    console.log("西四区-大西洋时间（加拿大）：" + getLocalTime(-4));
-    console.log("西三区-巴西利亚时间：" + getLocalTime(-3));
+
+    // console.log("*******************东区时间************************************");
+    // console.log("零时区-伦敦时间：" + getLocalTime(0));
+    // console.log("东一区-柏林时间：" + getLocalTime(1));
+    // console.log("东二区-雅典时间：" + getLocalTime(2));
+    // console.log("东三区-莫斯科时间：" + getLocalTime(3));
+    // console.log("东四区-时间：" + getLocalTime(4));
+    // console.log("东五区-伊斯兰堡时间：" + getLocalTime(5));
+    // console.log("东六区-科伦坡时间：" + getLocalTime(6));
+    // console.log("东七区-曼谷时间：" + getLocalTime(7));
+    // console.log("东八区-北京时间：" + getLocalTime(8));
+    // console.log("东九区-东京时间：" + getLocalTime(9));
+    // console.log("东十区-悉尼时间：" + getLocalTime(10));
+    // console.log("东十二区-斐济时间：" + getLocalTime(12));
+    // console.log("*******************西区时间************************************");
+    // console.log("西十区-斐济时间：" + getLocalTime(-10));
+    // console.log("西九区-阿拉斯加时间：" + getLocalTime(-9));
+    // console.log("西八区-太平洋时间（美国和加拿大）：" + getLocalTime(-8));
+    // console.log("西七区-山地时间（美国和加拿大）：" + getLocalTime(-7));
+    // console.log("西六区-中部时间（美国和加拿大）：" + getLocalTime(-6));
+    // console.log("西五区-东部时间（美国和加拿大）：" + getLocalTime(-5));
+    // console.log("西四区-大西洋时间（加拿大）：" + getLocalTime(-4));
+    // console.log("西三区-巴西利亚时间：" + getLocalTime(-3));
 }
 
 // 得到标准时区的时间的函数
 function getLocalTime(i, date) {
+
+    // if (date) {
+    console.log('date been is ', date)
+    // }
 
     //参数i为时区值数字，比如北京为东八区则输进8,西5输入-5
     if (typeof i !== 'number') return;
