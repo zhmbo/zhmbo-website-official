b3770513a6606bcf5b3d0849d4a1fe8f0bfcad17
itzhangbao <itzhangbao@163.com>
2020-10-03 16:15:41 +0800

    jumbo`official update 🚀

--- application/babel/templates/blog.js
+++ application/babel/templates/blog.js
@@ -2,7 +2,8 @@ function blog() {
     setTimeout(function () {
         Timer.run('.template[data-template=blog] time',
             function () {
-                // Router.route('#')
+                Identity.work()
+                window.location.href = '#'
                 window.location.href = "blog"
             }, 5)
     }, Identity.duration * 1.25)
